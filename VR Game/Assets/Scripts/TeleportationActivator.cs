3eca6d55d833e4838205dc3326a56c2a91924b2a
holovanist <jonsonlogan37@gmail.com>
2026-04-22 13:36:59 -0700

    added tmp and made ui work

--- VR Game/Assets/Scripts/TeleportationActivator.cs
+++ VR Game/Assets/Scripts/TeleportationActivator.cs
@@ -5,22 +5,29 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty teleportActivatorAction;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
-
         teleportActivatorAction.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => DisableTeleportRay());
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if(rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            return;
+        }
         teleportInteractor.gameObject.SetActive(true);
     }
+    public void DisableTeleportRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+    }
 
-    // Update is called once per frame
     void Update()
     {
         if(teleportActivatorAction.action.WasReleasedThisFrame())
