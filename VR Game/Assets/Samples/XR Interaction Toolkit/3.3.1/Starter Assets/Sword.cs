7631bf89f68feb6ad765e1ccc5fcf8ecd153b1d5
holovanist <jonsonlogan37@gmail.com>
2026-04-30 13:39:53 -0700

    working on swords

---
+++ VR Game/Assets/Samples/XR Interaction Toolkit/3.3.1/Starter Assets/Sword.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    Vector3 enterPosition;
+    Vector3 exitPosition;
+    float time;
+    public float MaxTime;
+    public float Damage;
+    public Vector3 temp;
+    private void OnTriggerEnter(Collider other)
+    {
+        time = 0;
+        enterPosition = transform.position;
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Trigger");
+            temp = GetTotalMeshFilterBounds(other.transform).size;
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        time += Time.deltaTime;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        CalculateDamage(time);
+        exitPosition = transform.position;
+        
+    }
+    public void CalculateDamage(float timer)
+    {
+        float distance = Vector3.Distance(enterPosition, exitPosition);
+        if(timer > MaxTime)
+        {
+            timer = MaxTime;
+        }
+        float damageDelt = Damage * (distance);
+        Debug.Log(damageDelt);
+    }
+
+    private static Bounds GetTotalMeshFilterBounds(Transform objectTransform)
+    {
+        var meshFilter = objectTransform.GetComponent<MeshFilter>();
+        var result = meshFilter != null ? meshFilter.mesh.bounds : new Bounds();
+
+        foreach (Transform transform in objectTransform)
+        {
+            var bounds = GetTotalMeshFilterBounds(transform);
+            result.Encapsulate(bounds.min);
+            result.Encapsulate(bounds.max);
+        }
+        var scaledMin = result.min;
+        scaledMin.Scale(objectTransform.localScale);
+        result.min = scaledMin;
+        var scaledMax = result.max;
+        scaledMax.Scale(objectTransform.localScale);
+        result.max = scaledMax;
+        return result;
+    }
+}
